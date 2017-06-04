2d7216cbd57c25325eabbf5d9ae90ce95f98e57c
Premsjce <Premsjce@gmail.com>
2017-06-05 02:55:46 +0530

    Added INotifyPropertyChanged Implementation demo

---
+++ INotifyPropertyChangedDemo/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyPropertyChangedDemo
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public MainViewModel()
+        {
+            Intialize();
+        }
+
+        private void Intialize()
+        {
+            this._checkboxstate = false;
+            this.checkBoxEnabled = false;
+        }
+
+        private bool _checkboxstate ;
+
+        public bool CheckBoxState
+        {
+            set
+            {
+                _checkboxstate = value;
+                CheckBoxEnabled = value;
+                RaisePropertyChanged("CheckBoxState");
+            }
+        }
+
+
+        private bool checkBoxEnabled;
+
+        public bool CheckBoxEnabled
+        {
+            get
+            {
+                return checkBoxEnabled;
+            }
+            set
+            {
+                checkBoxEnabled = value;
+                RaisePropertyChanged("CheckBoxEnabled");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));            
+        }
+
+    }
+}
