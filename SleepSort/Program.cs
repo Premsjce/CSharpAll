400722f08eb53f8f1848664447e7addb89d42da6
Prem <premsjce@gmail.com>
2018-09-16 00:27:06 +0530

    Added the sleepsort algorithm

---
+++ SleepSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace SleepSortCSharp
+{
+    class SleepSort
+    {
+        static void Main(string[] args)
+        {
+            int[] _inputArray = { 100, 56, 32, 79, 1, 0, 55, 92, 55, 56 };
+
+            foreach (int _temp in _inputArray)
+            {
+                Thread thread = new Thread(PrintNumber);
+                thread.Start(_temp);
+            }
+        }
+
+        static void PrintNumber(object num)
+        {
+            int number = (int)num;
+            Thread.Sleep(number);
+            Console.WriteLine(number);
+        }
+    }
+}
